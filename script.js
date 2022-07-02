9bd04dcbce271132905711c7348dc80d590bd4a4
vibovenkat123 <vibovenkat@outlook.com>
2022-07-02 17:01:05 -0500

    rollback changes

--- script.js
+++ script.js
@@ -1,35 +1,35 @@
-class Calculator{
-  constructor(previousOperandText, currentOperandText) {
-    this.previousOperandText = previousOperandText;
-    this.currentOperandText = currentOperandText;
-    this.clear();
-  }
-  addToScreen(num) {
-    
-  }
-  clear() {
-    this.currentOperand = '';
-    this.previousOperand = '';
-    this.operation = null;
-  }
-  pickOperation(operation) {
-  
-  }
-  compute() {
-  
-  }
+const add = (num1, num2) => {
+  return num1 + num2;
+};
 
-  updateDisplay() {
-  
+const subtract = (num1, num2) => {
+  return num1 - num2;
+};
+
+const multiply = (num1, num2) => {
+  return num1 * num2;
+};
+
+const divide = (num1, num2) => {
+  return num1 / num2;
+};
+
+const operate = (operator, num1, num2) => {
+  switch (operator) {
+    case "+":
+      return add(num1, num2);
+
+    case "/":
+      if (num2 == 0) {
+        return "ERROR";
+      } else {
+        return divide(num1, num2);
+      }
+
+    case "-":
+      return subtract(num1, num2);
+
+    case "*":
+      return multiply(num1, num2);
   }
-  
-}
-
-const numberBtn = document.querySelectorAll["[data-number]"];
-const operatorBtn = document.querySelectorAll["[data-operator]"];
-const equalsBtn = document.querySelector["[data-equals]"];
-const clearBtn = document.querySelector["[data-clear]"];
-const previousOperandText = document.querySelector["[data-previous-op]"];
-const currentOperandText = document.querySelector["[data-current-op]"];
-const display = document.getElementsByClassName;
-
+};
