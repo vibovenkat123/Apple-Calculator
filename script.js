80412379b09de0609b40a8f2a597ef9d51b9b921
vibovenkat123 <vibovenkat@outlook.com>
2022-07-02 12:56:44 -0500

    added operate function added function which takes a operator, and does the add, subtract, divide, or multiply function based on the operator

--- script.js
+++ script.js
@@ -2,6 +2,23 @@ const add = (num1, num2) => {return num1+num2};
 const subtract = (num1, num2) => {return num1 - num2};
 const multiply = (num1, num2) => {return num1 * num2};
 const divide = (num1, num2) => {return num1 / num2};
+const operate = (operator, num1, num2) =>{
+  switch (operator) {
+    case '+':
+      return add(num1, num2);
+    case '/':
+      if (num2 == 0){
+        return 'ERROR';
+      }
+      else{
+        return divide(num1, num2);
+      }
+    case '-':
+      return subtract(num1, num2);
+    case 'x':
+      return multiply(num1, num2);
+  }
+}
 
 
 
