088c56774b2da65e0470206e16d69dc195510cb5
vibovenkat123 <vibovenkat@outlook.com>
2022-07-03 10:37:30 -0500

    add display function updates the display if you press a digit

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+const decimal = document.getElementById("decimal");
+const equals = document.getElementById("equal-sign");
+const clear = document.getElementById("all-clear");
+const operand = document.getElementById("operand");
+const digits = document.querySelectorAll("button");
+const button = document.querySelectorAll("button");
 const add = (num1, num2) => {
   return num1 + num2;
 };
@@ -33,3 +39,13 @@ const operate = (operator, num1, num2) => {
       return multiply(num1, num2);
   }
 };
+let operator = '';
+
+for (const digit of digits) {
+  digit.addEventListener("click", () => {
+    if (digit.classList.contains("number")) {
+      operand.textContent = digit.textContent;
+    }
+  });
+}
+
