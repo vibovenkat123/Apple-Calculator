497e0d6e77e73e64d98bdca7c8d5c2ed16c26585
vibovenkat123 <vibovenkat@outlook.com>
2022-07-03 18:47:21 -0500

    fixed decimal

--- mobile_script.js
+++ mobile_script.js
@@ -1,8 +1,9 @@
+
 const equals = document.getElementById("equal-sign");
 const clear = document.getElementById("all-clear");
 const operand = document.getElementById("operand");
 const digits = document.querySelectorAll(".number");
-let sign = "";
+let sign = '';
 const operator = document.querySelectorAll(".operator");
 let firstNumber = "";
 let secondNumber = "";
@@ -12,8 +13,8 @@ const operate = () => {
   switch (sign) {
     case "+":
       let sum = bothNumbers.reduce((previous, current) => {
-        return current + previous;
-      });
+        return current+previous;
+      }, )
       firstNumber = sum;
       secondNumber = "";
       operand.innerText = sum;
@@ -37,8 +38,8 @@ const operate = () => {
 
     case "-":
       let difference = bothNumbers.reduce((previous, current) => {
-        return previous - current;
-      });
+        return previous-current;
+      })
       firstNumber = difference;
       secondNumber = "";
       operand.innerText = difference;
@@ -47,72 +48,79 @@ const operate = () => {
       break;
     case "x":
       let product = bothNumbers.reduce((previous, current) => {
-        return current * previous;
-      });
+        return current*previous;
+      })
       firstNumber = product;
       secondNumber = "";
       operand.innerText = product;
       bothNumbers = [];
       clearScreen = true;
       break;
+      
   }
 };
 
 digits.forEach((digit) => {
   digit.addEventListener("click", () => {
+    
     if (sign == "") {
-      if (clearScreen) {
+      if (clearScreen){
         clearScreen = false;
-        operand.textContent = "";
+        operand.textContent = '';
         firstNumber += digit.textContent;
-        firstNumber = parseInt(firstNumber);
-        operand.textContent += digit.textContent;
-      } else {
-        firstNumber += digit.textContent;
-        firstNumber = parseInt(firstNumber);
+        firstNumber = parseInt(firstNumber)
         operand.textContent += digit.textContent;
       }
-    } else {
-      if (clearScreen) {
-        operand.textContent = "";
-        secondNumber += digit.textContent;
-        clearScreen = false;
-        secondNumber = parseInt(secondNumber);
-        operand.textContent += digit.textContent;
-      } else {
-        secondNumber += digit.textContent;
-        secondNumber = parseInt(secondNumber);
+      else {
+        firstNumber += digit.textContent;
+        firstNumber = parseInt(firstNumber)
         operand.textContent += digit.textContent;
       }
+    }
+    else {
+      if (clearScreen){
+        operand.textContent = '';
+        secondNumber += digit.textContent;
+        clearScreen = false;
+        secondNumber = parseInt(secondNumber)
+        operand.textContent += digit.textContent;
+      }
+      else{
+        secondNumber += digit.textContent;
+        secondNumber = parseInt(secondNumber)
+        operand.textContent += digit.textContent;
+      }
+      
     }
   });
 });
 
 clear.addEventListener("click", () => {
   operand.textContent = "";
-  firstNumber = "";
-  secondNumber = "";
+  firstNumber = '';
+  secondNumber = '';
   bothNumbers = [];
-  sign = "";
+  sign = '';
 });
-equals.addEventListener("click", () => {
-  operand.textContent = "";
+equals.addEventListener('click', () => {
+  operand.textContent = '';
   operate();
-});
+})
 
-operator.forEach((operators) => {
-  operators.addEventListener("click", () => {
-    if (operators.id != "equals-sign") {
-      if (sign != "") {
-        operand.textContent = "";
-        operate();
-        clearScreen = true;
-        sign = operators.innerText;
-      } else {
-        operand.textContent = "";
-        sign = operators.innerText;
+operator.forEach(operators => {
+  operators.addEventListener('click', () => {
+    if (operators.id != 'equals-sign'){
+      if (sign != ""){
+          operand.textContent = '';
+          operate();
+          clearScreen = true;
+          sign = (operators.innerText);
+      }
+      else {
+        operand.textContent = '';
+        sign = (operators.innerText);
         operand.textContent = "";
       }
     }
-  });
-});
+  })
+})
